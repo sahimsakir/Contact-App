0fb0a420fa745c904fdde1b7884b0f031379373a
sahimsakir <sahimsakir@gmail.com>
2023-05-08 15:42:42 +0600

    Database Schema

---
+++ app/Models/Company.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name','address','website','email'];
+
+    public function contacts(){
+        return $this->hasMany(Contact::class, 'id_company');
+    }
+}
