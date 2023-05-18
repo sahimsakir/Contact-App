5870d9fced1acdec9b53769ee9473a5516ce72a4
sahimsakir <sahimsakir@gmail.com>
2023-05-18 12:39:15 +0600

    API Resources

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\ContactController;
+use App\Http\Controllers\API\CompanyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::apiResources([
+    'contacts'=> ContactController::class,
+    'companies'=> CompanyController::class,
+]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
